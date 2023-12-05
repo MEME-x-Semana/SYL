45725c35882c620c105479654a7ce95e547ef5a3
Flavia S. Briglia <fbrig87@gmail.com>
2023-12-05 15:38:17 -0300

    cre y modifica los patrones en views y urls

--- syl_app/views.py
+++ syl_app/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
 
-# Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.views.generic.detail import DetailView
+
+
+from .models import Cruceros
+
+# Basicamente creo el CRUD por medio de clases
+
+class CrucerosBaseView(View):
+    template_name = 'cruceros.html'
+    model = Cruceros
+    fields = '__all__'
+    success_url = reverse_lazy('cruceros:all')
+
+
+class CrucerosListView(CrucerosBaseView,ListView):
+    """
+    ESTO ME PERMITE CREAR UNA VISTA CON LOS VIAJES
+    """
+    ...
+
+class CrucerosDetailView(CrucerosBaseView,DetailView):
+    template_name = "crucero_detail.html"
+
+class CrucerosCreateView(CrucerosBaseView,CreateView):
+    template_name = "crucero_create.html"
+    extra_context = {
+        "tipo": "Crear viaje"
+    }
+
+
+class CrucerosUpdateView(CrucerosBaseView,UpdateView):
+    template_name = "crucero_create.html"
+    extra_context = {
+        "tipo": "Actualizar viaje"
+    }
+
+class CrucerosDeleteView(CrucerosBaseView,DeleteView):
+    template_name = "crucero_delete.html"
+    extra_context = {
+        "tipo": "Borrar viaje"
+    }
+
