eb023baf413696e22db9b3455c29f1db98ba197e
Flavia S. Briglia <fbrig87@gmail.com>
2023-12-05 14:34:01 -0300

    config de startapp: admin

--- syl_app/admin.py
+++ syl_app/admin.py
@@ -1,3 +1,9 @@
+from .models import Cruceros
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 # Register your models here.
+
+@admin.register(Cruceros)
+class CrucerosAdmin(ModelAdmin):
+    ...
