0e3179014d53460f92e095660af14b438c094cb2
Flavia S. Briglia <fbrig87@gmail.com>
2023-12-05 10:24:27 -0300

    elimina y reinstala venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\axeli\Documents\codes\SYL\.venv
+set VIRTUAL_ENV=C:\Users\Lujan\Desktop\cac_TIF\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
