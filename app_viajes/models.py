9e64cf7d2ac302b394987deebfca87d56bd5d212
Fabricio Barreto <fabrib40@gmail.com>
2023-12-06 21:42:11 -0300

    fabricio - connected BD to mysql

--- app_viajes/models.py
+++ app_viajes/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
 class Viajes(models.Model):
-    
-    nombre  =  models.CharField(max_length=200)
-    rating  = models.PositiveSmallIntegerField(blank=False,null=False)
-    abv     = models.FloatField(blank=True, null=True)
+    nombre = models.CharField(
+        max_length=200, null=False, unique=True, verbose_name="Nombre"
+    )
+    rating = models.PositiveSmallIntegerField(blank=False, null=False)
+    abv = models.FloatField(blank=True, null=True)
 
-    
     class Meta:
         db_table = "viajes_table"
-
+        verbose_name = "Viaje"
+        verbose_name_plural = "Viajes"
+        ordering = ["id"]
 
     def __str__(self):
-        return f"El vino: {self.nombre}, Rating {self.abv}"
+        return f"Viaje: {self.nombre}, Rating: {self.abv}"
 
     def get_fields(self):
         return [
