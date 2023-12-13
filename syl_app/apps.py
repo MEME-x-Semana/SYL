fddfa4c9119455e8d04c6c83b9981fb5827133d9
Axeli <axelivancriado@gmail.com>
2023-12-13 11:09:52 -0300

    Eliminiacion de una de las apps y ordenmaitno de app_viajes

--- syl_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class AppViajesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_viajes'
-
-class SylAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'syl_app'
